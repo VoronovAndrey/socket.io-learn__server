783e0fefaf4aca050da4fd0eb844d0ea0b8108d1
AndriiVoronov <andrii.voronov.work@gmail.com>
2022-11-09 01:23:19 +0100

    Create rooms

--- index.js
+++ index.js
@@ -15,8 +15,17 @@ const io = new Server(server, {
     }
 })
 
+let users = []
+let rooms = []
+
 io.on("connection", (socket) => {
     console.log(`user connected: ${socket.id}`);
+
+    socket.on('new_user', (data) => {
+        console.log('new_user', data);
+        users.push(data)
+        io.emit('new_user_response', users);
+    })
 
     socket.on('send_message', (data) => {
         // console.log('send_message: ',data);
@@ -24,6 +33,31 @@ io.on("connection", (socket) => {
         socket.emit('recive_message', data)
         socket.broadcast.emit('recive_message', data)
     })
+
+    socket.on('create_room', (data) => {
+        rooms.push(data)
+        console.log('room added', rooms);
+        
+        let tmp = [] 
+        data.members.map(item => {
+            let userInfo = users.filter(_user => _user._id === item)
+            if (userInfo.length > 0) {
+                tmp.push(userInfo[0])
+            }
+        })
+        tmp.map(user => {
+            io.to(user.socket_id).emit('new_chat_response', rooms)
+        })
+    })
+
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected');
+        //Updates the list of users when a user disconnects from the server
+        users = users.filter((user) => user.socket_id !== socket.id);
+        //Sends the list of users to the client
+        io.emit('new_user_response', users);
+        socket.disconnect();
+      });
 })
 
 
